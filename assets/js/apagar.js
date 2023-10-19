2e7d8ab4e29a927d61836a7163f4bb2fff1edaaa
Leonardo1805 <leonardo.feitosadasilva@yahoo.com.br>
2023-10-18 22:11:02 -0300

    ArraysJavaScript

---
+++ assets/js/apagar.js
@@ -0,0 +1,13 @@
+const botoes = document.querySelectorAll(".btn-apagar")
+
+
+function apagar(id){
+    console.log("Apagar tarefa", id)
+    //buscar os dados do localstorage
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+    //remover a tarefa
+    const tarefas_filtradas = tarefas.filter(tarefa => tarefa.id !== id)
+    //salvar o localstorage
+    localStorage.setItem("tarefas", JSON.stringify(tarefas_filtradas))
+    atualizar()
+}
